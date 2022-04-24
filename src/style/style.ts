77728e4f28c7d69585b5671e6f110ca2b96775d4
Velnovikova <leranovikova21091998@gmail.com>
2022-04-24 16:01:28 +0300

    add form on page profile

--- src/style/style.ts
+++ src/style/style.ts
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { style } from '@mui/system';
 export const SearchMenu = styled.div`
 display:flex;
 flex-direction:column;`
@@ -38,8 +39,39 @@ export const TagList = styled.section`
 display:flex;
 text-transform: capitalize;
 `
-export const ErrorText=styled.p`
-color:red`
+export const ErrorText = styled.p`
+color:red;`
+export const FormContent = styled.div`
+
+`
+export const InputText = styled.input`
+  display: block;
+  box-sizing: border-box;
+  width: 100%;
+  border-radius: 4px;
+  border: 1px solid black;
+  padding: 10px 15px;
+  margin-bottom: 10px;
+  font-size: 14px;`
+export const Label = styled.label`
+  line-height: 2;
+  text-align: left;
+  display: block;
+  margin-bottom: 13px;
+  margin-top: 20px;
+  color: black;
+  font-size: 16px;
+  font-weight: 400;`
+export const ButtonSubmit=styled.button`
+background: black;
+  color: white;
+  text-transform: uppercase;
+  border: none;
+  margin-top: 40px;
+  padding: 20px;
+  font-size: 16px;
+  font-weight: 700;
+  letter-spacing: 10px;`
 export const FooterSt = styled.footer`
 color:white;
 background-color:black;
